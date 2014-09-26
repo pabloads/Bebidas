c4802a64de3c2a2610a8b259ad367878d34bf22a
Pablo Rocha <pabloferreirarocha@hotmail.com>
2014-09-26 13:09:21 -0300

    Hello World

--- src/java/controle/HelloControler.java
+++ src/java/controle/HelloControler.java
@@ -7,7 +7,9 @@
 package controle;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.portlet.ModelAndView;
 
 /**
@@ -15,10 +17,18 @@ import org.springframework.web.portlet.ModelAndView;
  * @author pablo.ferreira
  */
 @Controller
+
 public class HelloControler {
-    @RequestMapping("/view")
- public ModelAndView hello() {
- 		String message = "Welcome to Spring MVC 4.0!";
- 		return new ModelAndView("view", "message", message);
- 	}
+//    @RequestMapping(value = "/view", method = RequestMethod.GET)
+// public ModelAndView view() {
+// 		String message = "Welcome to Spring MVC 4.0!";
+// 		return new ModelAndView("view", "message", message);
+// 	}
+    
+   @RequestMapping("/")
+    public String printHelloWorld(Model model) {
+        model.addAttribute("message", "Hello World!");
+ 
+        return "ola";
+    }
 }
